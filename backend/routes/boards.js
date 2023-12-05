5280136afe950e669cbd3cf2fd775c888ab0bbdc
michael xi <michael.xi1029@gmail.com>
2023-12-05 00:22:04 -0500

    updated board.js for delete and addmember

--- backend/routes/boards.js
+++ backend/routes/boards.js
@@ -6,10 +6,17 @@ const Board = require('../models/Board');
 const BoardMember = require('../models/BoardMember')
 const authenticateToken = require('../middleware/authenticateToken');
 
+
+
 // create board
 router.post('/createBoard', authenticateToken, async (req, res) => {
     try {
+        
         const {name} = req.body;
+
+        if (await Board.findOne({ name: name})) {
+            return res.status(400).send('boardname already exists');
+          }
         const newBoard = new Board({ name: name });
         const savedBoard = await newBoard.save();
 
@@ -25,24 +32,109 @@ router.post('/createBoard', authenticateToken, async (req, res) => {
 });
 
 // delete board
+router.delete('/deleteBoard', authenticateToken, async (req, res) => {
+    try {
+        const {boardname} = req.body;
+        const board = await Board.findOne({ name: boardname});
+
+        // Check if the user has the necessary permissions to delete the board
+        const user = await User.findOne({ username: req.user.username });
+        const isAdmin = await BoardMember.findOne({ user: user._id, board: board._id, isAdmin: true });
+
+        if (!isAdmin) {
+            return res.status(403).send( 'You do not have permission to delete the board.' );
+        }
+
+        // Delete the board
+        const deletedBoard = await Board.findOneAndDelete( { _id: board._id } );
+
+        if (!deletedBoard) {
+            return res.status(404).send('Board not found.' );
+        }
+
+        // Also delete related board members
+        await BoardMember.deleteMany({ board: board._id });
+
+        res.status(200).send( 'Board deleted successfully.' );
+
+    } catch (error) {
+        console.error('Error fetching user information:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
 // add member
-// router.post('/addMember', authenticateToken, async (req, res) => {
-//     try {
-//         // check if user is admin
-//         const {username, boardname} = req.body; // username of person to add
-//         // 
+router.post('/addMember', authenticateToken, async (req, res) => {
+    try {
+        
+        const {username, boardname} = req.body; // username of person to add
+        const user_to_add = await User.findOne({ username: username});
+        const user = await User.findOne({ username: req.user.username});
+        const board = await Board.findOne({ name: boardname}); 
+        // chedk if board exists
+        if (!board) {
+            return res.status(404).send('board does not exist');
+          }
+        //check if user exists
+        if (!user_to_add) {
+            return res.status(404).send('user does not exist');
+          }
+        // check if user is already a member
+        if (await BoardMember.findOne({ user: user_to_add._id, board: board._id})) {
+            return res.status(400).send("already a member");
+          }
+        // check if its an admin calling the function
+        const isAdmin = await BoardMember.findOne({user: user._id, board: board._id, isAdmin: true});
+        if (!isAdmin) {
+            res.status(400).send('you are not an admin');
+        }
 
-//     } catch (error) {
-//         console.error('Error fetching user information:', error);
-//         res.status(500).send('Internal Server Error');
-//     }
-// });
+        //add member
+        
+        const newBoardMember = new BoardMember({user: user_to_add._id, board: board._id, isAdmin: false});
+        const savedBoardMember = await newBoardMember.save();
+        res.json(newBoardMember);
+
+
+    } catch (error) {
+        console.error('Error fetching user information:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
 // remove member
 
 // leave
+router.delete('/leaveBoard', authenticateToken, async (req, res) => {
+    try {
+        const {boardname} = req.body;
+        const board = await Board.findOne({ name: boardname});
+        if (!board) {
+            return res.status(404).send('Board not found.' );
+        }
+        // Check if the user has the necessary permissions to delete the board
+        const user = await User.findOne({ username: req.user.username });
+        const boardMember = await BoardMember.findOne({ user: user._id, board: board._id });
+        
+        if (!boardMember) {
+            return res.status(403).send('You are not a member of this board.');
+        }
 
+        // Check if the user is the owner (admin)
+        if (boardMember.isAdmin) {
+            return res.status(403).send('You cannot leave as the owner.');
+        }
+
+        //leave the board
+        const leaveboard = await BoardMember.deleteOne({ user: user._id, board: board._id });
+
+        res.status(200).send( 'Left board successfully.' );
+
+    } catch (error) {
+        console.error('Error fetching user information:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 // add channel
 
 // remove channel
