8ea16cfb5d87ea7b3c88edd530673e845a6b50ef
allyzwy <ally.zhang@mail.mcgill.ca>
2023-12-06 14:56:38 -0500

    trying to fix visualizing navbar on select.js

--- frontend/src/components/DiscussionBoard.jsx
+++ frontend/src/components/DiscussionBoard.jsx
@@ -15,4 +15,6 @@ const DiscussionBoard = ({ boardName }) => {
   );
 };
 
+// make it such that clicking on a board component leads to TMP.js page with that *boardName or id as input?*
+// sidebar and messaging.js depend on TMP.js
 export default DiscussionBoard;
