511fb3aafca2b15529875444253be655f871a0e3
michael xi <michael.xi1029@gmail.com>
2023-12-11 23:55:43 -0500

    Update channels.js

--- backend/routes/channels.js
+++ backend/routes/channels.js
@@ -21,13 +21,47 @@ router.get('/getChannel', authenticateToken, async (req, res) => {
         res.status(500).send('Internal Server Error');
     }
 });
+router.get('/getChannel/:channelid', authenticateToken, async (req, res) => {
+    try {
+        const { channelid } = req.params;
+        const currChannel = await Channel.findById({ _id: channelid });
+        if (!currChannel) {
+            return res.status(404).json({ error: 'Channel not found' });
+        }
+        const channelName = currChannel.channelname;
+        
+        res.json(channelName);
+
+    } catch (error) {
+        console.error('Error fetching user information:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 
 router.post('/createChannel', authenticateToken, async (req, res) => {
    try {
     // console.log("BONK")
-    const { channelName } = req.body;
-    const newChannel = new Channel({name: channelName});
+    const { channelname, boardid } = req.body;
+    console.log(channelname, boardid);
+    const channelexist = await Channel.findOne({ name: channelname }); 
+    const board = await Board.findById({ _id: boardid });
+    if (channelexist) {
+        return res.status(400).send('Channel already exists');
+    }
+    if (!board) {
+        return res.status(404).send('Board not found');
+    }
+    const newChannel = new Channel({name: channelname});
     const savedChannel = await newChannel.save();
+    
+    const newBoardChannel = new BoardChannel({channel: savedChannel._id, board: board._id});
+    const savedBoardChannel = await newBoardChannel.save();
+    if (!savedChannel || !savedBoardChannel) {
+        return res.status(400).send('Error creating channel');
+    }
+
+
     res.status(201).json(savedChannel);
 
    } catch (error) {
@@ -35,7 +69,6 @@ router.post('/createChannel', authenticateToken, async (req, res) => {
     res.status(500).send('Internal Server Error');
 }
 });
-
 // rename channel
 
 // delete channel
