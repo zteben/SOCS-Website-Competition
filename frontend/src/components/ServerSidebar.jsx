e9010360cf251f423ac078ed61dc329ba4e3186e
michael xi <michael.xi1029@gmail.com>
2023-12-11 23:56:27 -0500

    Update ServerSidebar.jsx

--- frontend/src/components/ServerSidebar.jsx
+++ frontend/src/components/ServerSidebar.jsx
@@ -1,33 +1,108 @@
-import React from 'react';
-import './ServerSidebar.css';
-import { SERVERSidebarData } from './ServerSidebarData';
-// Placeholder channels data
-const Mysidebar = {
-  height: '100vh',
-  width: '17rem',
-  overflowX: 'hidden',
-  overflowY: 'scroll',
-};
-export default function serverSidebar({ serversidebar }) {
+import React, { useEffect, useState } from 'react';
+import "./ServerSidebar.css";
+import ChannelBox from './ChannelBox';
+import { Link } from 'react-router-dom';
+import NewChannel from './NewChannel';
+
+const ServerSidebar = ({ boardid }) => {
+  const [channels, setChannels] = useState([]);
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  const board_id = boardid;
+  const token = localStorage.getItem('accessToken');
+  
+  useEffect(() => {
+    const fetchChannels = async () => {
+      try {
+        console.log(board_id);
+        console.log(token);
+        const response = await fetch(
+          `http://localhost:3000/channels/getAllChannel/${board_id}`,
+          {
+            method: 'GET',
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+
+        const data = await response.json();
+
+        setChannels(data);
+        console.log('Fetched channels:', data);
+      } catch (error) {
+        console.error('Error fetching channels:', error);
+      }
+    };
+    fetchChannels();
+    
+  }, [board_id, token]);
+  
+
+  // useEffect(() => {
+  //   const checkAdminStatus = async () => {
+  //     try {
+  //       const response = await fetch(
+  //         `http://localhost:3000/isAdmin/checkAdminStatusById?_id=${board_id}`,
+  //         {
+  //           method: 'POST',
+  //           headers: {
+  //             'Content-Type': 'application/json',
+  //             Authorization: token,
+  //           },
+
+  //         }
+  //       );
+
+  //       if (!response.ok) {
+  //         throw new Error(`HTTP error! Status: ${response.status}`);
+  //       }
+
+  //       const data = await response.json();
+  //       setIsAdmin(data.isAdmin);
+  //     } catch (error) {
+  //       console.error('Error checking admin status:', error);
+  //     }
+  //   };
+
+  //   checkAdminStatus();
+  // }, [board_id, token]);
+
+
   return (
-    <>
-      <nav className={serversidebar ? 'nav-menu active' : 'nav-menu'}>
-        <div className="nav-title">
-          <p>CHANNELS</p>
-        </div>
-        {/* Render each channel */}
-        <ul className="nav-menu-items">
-          <div style={Mysidebar}>
-            {SERVERSidebarData.map((item, index) => {
-              return (
-                <li key={index} className={item.class}>
-                  <span>{item.title}</span>
-                </li>
-              );
-            })}
+    
+    <> 
+      <div className="scroller">
+        <div className="nav-menu">
+          <div className="nav-title">
+            <p>CHANNELS</p>
+            <NewChannel boardid={board_id} />
+            
+      
           </div>
-        </ul>
-      </nav>
+          <ul className="nav-menu-items">
+            
+            {
+              channels.map((channel,index) => (
+              
+                <Link key={channel.channel} to={`/${board_id}/${channel.channel}`}>
+                  <ChannelBox ChannelId={channel.channel} isAdmin={isAdmin} />
+                </Link>
+                
+              ))
+        }
+
+          </ul>
+        </div>
+      </div>
     </>
   );
-}
+};
+
+export default ServerSidebar;
